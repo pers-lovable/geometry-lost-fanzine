84900fb224c58176c45722e31d5a1155f225dd07
Per Weijnitz <per.weijnitz@gmail.com>
2024-02-17 15:48:18 +0100

    Unhardcode paths

--- org2pdf.el
+++ org2pdf.el
@@ -2,13 +2,20 @@
   (interactive)
   (customize-set-variable 'org-export-with-sub-superscripts nil)
   (customize-set-variable 'org-export-with-emphasize nil)
-  (let ((default-directory data-dir))
+  ;; Why copy all source files to the output directory first?
+  ;; At export time, the current working directory needs to be the output directory.
+  ;; At export time, all org-links and includes need to be resolvable.
+  ;; So unless we want to hard code directory names in org-modes links and includes
+  ;; to point to the source directory, we can solve it by copying the source files
+  ;; into the output directory.
+  (shell-command (concat "rsync -va --protect-args " data-dir "/* " output-dir "/"))
+
+  (let ((default-directory output-dir))
     (find-file report-source-file)
     (set-buffer report-source-file)
     (add-to-list 'org-latex-default-packages-alist "\\PassOptionsToPackage{hyphens}{url}")
     (customize-set-value 'org-latex-hyperref-template "\\hypersetup{\n pdfauthor={%a},\n pdftitle={%t},\n pdfkeywords={%k}, pdfsubject={%d},\n pdfcreator={%c},\n pdflang={%L},\n colorlinks=true,urlcolor=blue,linkcolor=blue}\n"))
-  (let ((default-directory output-dir))
-    (org-latex-export-to-pdf)))
+    (org-latex-export-to-pdf))
 
 
 (let*
